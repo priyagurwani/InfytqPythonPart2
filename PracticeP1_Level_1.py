4a15908cc279c775b4d45d20f52dcef011da6a98
priyagurwani <84342895+priyagurwani@users.noreply.github.com>
2022-01-16 20:31:47 +0530

    1 Practice problem Solved:)

---
+++ PracticeP1_Level_1.py
@@ -0,0 +1,15 @@
+
+def add_string(str1):
+  #start writing your code here
+  new_str=""
+  if(len(str1)>=3):
+      # print(str[-3:])
+      if(str1[-3:]!="ing"):
+          return new_str.join(str1 + "ing")
+      else:
+          return new_str.join(str1 + "ly")
+  else:
+      return str1
+
+str1="com"
+print(add_string(str1))
